9a61d10af1d552a4a7f92483c0992808bc368d6b
Sowmya Buddharaju <70256420+sowmya214@users.noreply.github.com>
2021-10-24 16:54:07 +0100

    changed name of dataset

--- main_function.m
+++ main_function.m
@@ -1,7 +1,7 @@
 %RUN THIS FOR DECISION TREE
 
 function decision_tree_learning(binary_variable)
-    c_table = readtable('classification_dataset.csv', 'ReadVariableNames',true, "PreserveVariableNames",true);
+    c_table = readtable('dataset.csv', 'ReadVariableNames',true, "PreserveVariableNames",true);
     columnIndicesToDelete = [1,2,17,19,20,21,22,23];
     c_table(:,columnIndicesToDelete) = [];
     c_table.Properties.VariableNames = {'SR', 'NR', 'TR', 'VR', 'SUR1', 'SUR2', 'SUR3', 'UR', 'FR', 'OR', 'RR', 'BR', 'MR', 'CR', 'Brown frogs'};
